92e4ef8c523524ff1c8d52aa55f8582eab9e8368
Elliotto201 <elliotto.furmanski@gmail.com>
2025-11-13 22:46:58 +0100

    Runtime/Window class added

---
+++ AlloyRuntime/Runtime.cs
@@ -0,0 +1,38 @@
+﻿using AlloyRuntime.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlloyRuntime
+{
+    internal static class Runtime
+    {
+        public static void Start(string windowTitle, int width, int height, Window.GameConfig config, string assetFolder, bool assetsInTextFormat, Window.WindowIcon icon = null)
+        {
+            var window = new Window(windowTitle, width, height, config, icon);
+
+            AssetLoader.Initialize(
+            new AssetEngineContext
+            {
+                AssetRootFolderPath = assetFolder,
+                IsYamlAssets = assetsInTextFormat,
+            },
+            new Dictionary<Type, AssetPostProcessor>
+            {
+
+            },
+            new Dictionary<Type, CustomAssetSerializer>
+            {
+
+            },
+            new Dictionary<Type, ResourceSerializer>
+            {
+                { typeof(Texture2D), new Texture2DSerializer() },
+            });
+
+            window.Run();
+        }
+    }
+}
---
+++ AlloyRuntime/Window.cs
@@ -0,0 +1,62 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlloyRuntime
+{
+    internal sealed class Window : GameWindow
+    {
+        private readonly double TickRate;
+        private double lastTickTime;
+
+        public Window(string windowTitle, int width, int height, GameConfig config, WindowIcon icon = null) : base(GameWindowSettings.Default, new NativeWindowSettings
+        {
+            Title = windowTitle,
+            ClientSize = new OpenTK.Mathematics.Vector2i(width, height),
+        })
+        {
+            if(icon != null)
+            {
+                Icon = new OpenTK.Windowing.Common.Input.WindowIcon(new OpenTK.Windowing.Common.Input.Image(icon.Width, icon.Height, icon.Data));
+            }
+
+            TickRate = 1 / config.TickRate;
+            lastTickTime = 0;
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            lastTickTime += args.Time;
+
+            while (lastTickTime > TickRate)
+            {
+                lastTickTime -= TickRate;
+                GameLoop.CallOnTick();
+            }
+            GameLoop.CallOnFrame((float)args.Time);
+        }
+
+        public sealed class GameConfig
+        {
+            public int TickRate;
+        }
+
+        public sealed class WindowIcon
+        {
+            public int Width;
+            public int Height;
+            public byte[] Data;
+
+            public WindowIcon(int width, int height, byte[] data)
+            {
+                Width = width; 
+                Height = height; 
+                Data = data;
+            }
+        }
+    }
+}
